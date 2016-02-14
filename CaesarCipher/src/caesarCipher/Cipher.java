1ef695cc93d06021974e6f03fd78bf14e888cf84
Ben Chen <chenb@sunysuffolk.edu>
2016-02-14 17:57:30 -0500

    Delete everything

--- CaesarCipher/src/caesarCipher/Cipher.java
+++
@@ -1,8 +0,0 @@
-package caesarCipher;
-
-public class Cipher {
-
-	public static void main(String[] args) {
-		System.out.println(CaesarCipher.cipher("At noon be in the conference room with your hat on for a surprise party. YELL LOUD!", 15));
-	}
-}
